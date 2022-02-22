d6831ddf8d7dadb6b83a0da2da03c5bcd028edc8
abdelmomen <abdelmomen1985@gmail.com>
2022-02-22 13:53:22 +0200

    init solid trial

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import type { Component } from "solid-js";
+import styles from "./App.module.css";
+
+const App: Component = () => {
+  return <div class={styles.app}>HELLO</div>;
+};
+
+export default App;
